5eef7acbf74b83eaa624250a7688aef4bf2a10cb
Mariya <mbeketova@rambler.ru>
2015-04-06 17:08:04 +0300

    Initial Commit

---
+++ LessonThree_Sort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonThree_Sort
+//
+//  Created by Admin on 06.04.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LessonThree_Sort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LessonThree_Sort
+//
+//  Created by Admin on 06.04.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
